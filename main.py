1fb4256eb26a51a99a30916d0a508f22ccf62bd1
Titulin Alexander <tito-1984@mail.ru>
2022-11-16 17:26:42 +0300

    test with the number of photos

--- main.py
+++ main.py
@@ -16,23 +16,36 @@ if __name__ == '__main__':
     vasya = YandexDisk(token)
     vasya.create_folder(user_id)
     list_for_json = [] # Список для записи в json-файл
-    # len_photos_list = len(photos)
-    # print(f'В профиле пользователя {user_id} содержится {len_photos_list} фотографий.\nПо умолчанию будет загружено 5 фото.')
-    # user_choice = input('Желаете загрузить другое количество? Да/Нет ').lower()
-    # default_value = 5
-    
-    # if user_choice == 'да':
-    #     user_count = int(input('Введите количество фотографий для загрузки на Яндекс Диск: '))
-
-    
+    len_photos_list = len(photos)
+    print(f'В профиле пользователя {user_id} содержится {len_photos_list} фотографий.\nПо умолчанию будет загружено 5 фото.')
+    user_choice = input('Желаете загрузить другое количество? Да/Нет ').lower()
+    default_value = 5
+    user_count = int(input('Введите количество фотографий для загрузки на Яндекс Диск: '))
+    count = 1
     for photo in tqdm(photos):
-        path = f"{user_id}/{photo['file_name']}"
-        url = photo['url']
-        vasya.upload_file_to_disk(path, url)
-        json_dict = {'file_name': photo['file_name'], 'size': photo['size']}
-        list_for_json.append(json_dict)
-        with open(f'Фото профиля - {user_id}.json', 'w') as file:
-            json.dump(list_for_json, file, indent=4)    
-        time.sleep(0.08)    
+        if user_choice == 'да' and count <= user_count:
+            path = f"{user_id}/{photo['file_name']}"
+            url = photo['url']
+            vasya.upload_file_to_disk(path, url)
+            json_dict = {'file_name': photo['file_name'], 'size': photo['size']}
+            list_for_json.append(json_dict)
+            with open(f'Фото профиля - {user_id}.json', 'w') as file:
+                json.dump(list_for_json, file, indent=4)    
+            time.sleep(0.08)
+            count += 1
+        elif count <= default_value:
+            path = f"{user_id}/{photo['file_name']}"
+            url = photo['url']
+            vasya.upload_file_to_disk(path, url)
+            json_dict = {'file_name': photo['file_name'], 'size': photo['size']}
+            list_for_json.append(json_dict)
+            with open(f'Фото профиля - {user_id}.json', 'w') as file:
+                json.dump(list_for_json, file, indent=4)    
+            time.sleep(0.08)
+            count += 1
+            
+    
+
+              
     pprint(f'json-файл заргуженных фото: {list_for_json}')
-    print(f'Фотографии загружены на Яндекс Диск в папку {user_id}')    
+    print(f'Фотографии загружены на Яндекс Диск в папку {user_id}')
